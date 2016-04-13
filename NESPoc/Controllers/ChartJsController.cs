18137034ded2bd80aecfb8165a414c49635575c3
Phillip Berglund <pberglund@sagepath.com>
2016-04-13 17:57:01 -0400

    broke charts out into 2 controllers, added Google charts, index and Declining sales so far, rest are still chartjs

---
+++ NESPoc/Controllers/ChartJsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NESPoc.Controllers
+{
+	public class ChartJsController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult DecliningSales()
+		{
+			return View();
+		}
+
+		public ActionResult PurchaseHistory()
+		{
+			return View();
+		}
+		public ActionResult PurchaseFluctuation()
+		{
+			return View();
+		}
+	}
+}
--- NESPoc/Controllers/HomeController.cs
+++ NESPoc/Controllers/HomeController.cs
@@ -12,6 +12,5 @@ namespace NESPoc.Controllers
 		{
 			return View();
 		}
-
 	}
 }
