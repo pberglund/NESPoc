1a6ddc6c058670b7b97b74a39e59c62fcd41fe1a
Phillip Berglund <pberglund@sagepath.com>
2016-04-14 15:29:47 -0400

    "working" updates to the GoogleHelpers.js

--- NESPoc/Scripts/GoogleHelpers.js
+++ NESPoc/Scripts/GoogleHelpers.js
@@ -1,13 +1,53 @@
 ﻿
-function configureChart(chartVar, contextId, chartFunc, data, options, updateId, randomized) {
+function toggleTrendLines(chart, dataVal, optionsVal) {
+
+    if (optionsVal.trendlines != null) {
+        optionsVal.trendlines = null;
+    } else {
+        optionsVal.trendlines = {
+            0: {
+                color: 'black',
+            }
+        };
+    }
+    console.log('trend draw');
+    drawChartHelper(chart, dataVal, optionsVal);
+}
+
+function configureChartWithTrendLines(chartVar, contextId, chartFunc, data, options, updateId, trendLineId, randomized) {
+    chartVar = configureChart(chartVar, contextId, chartFunc, data, options, updateId, randomized);
+    
+    if (trendLineId != null) {
+
+        $('#' + trendLineId).unbind().click(function (e) {
+            e.preventDefault();
+            toggleTrendLines(chartVar, data, options);
+                
+            });
+        
+    }
+    return chartVar;
+}
+
+function configureChart(chartVar, contextId, chartFunc, dataVar, optionsVar, updateId, randomized) {
 
     chartVar = new google.visualization[chartFunc](document.getElementById(contextId));
-    chartVar.draw(data, options);
+    console.log('configure draw');
+    drawChartHelper(chartVar, dataVar, optionsVar);
 
-    $('#' + updateId).click(function (e) {
-        e.preventDefault();
-        randomized(chartVar);
+    if (updateId != null) {
+
+        $('#' + updateId).unbind().click(function(e) {
+            e.preventDefault();
+            randomized(chartVar);
+        });
+    }
+
+    $(window).resize(function () {
+        console.log('resize draw');
+        drawChartHelper(chartVar, dataVar, optionsVar);
     });
+    
 
     return chartVar;
 }
@@ -21,14 +61,14 @@ function randomData(max) {
     return val;
 }
 
-function randomizePointOnChart(chart, data, options, max) {
+function randomizePointOnChart(chart, dataVar, optionsVar, max) {
     if (chart == null) {
         return;
     }
-    if (data == null) {
+    if (dataVar == null) {
         return;
     }
-    if (options == null) {
+    if (optionsVar == null) {
         return;
     }
     if (max == null || max < 0) {
@@ -37,31 +77,65 @@ function randomizePointOnChart(chart, data, options, max) {
 
     var value = Math.floor((Math.random() * max) + 1);
 
-    var dataSet = Math.floor((Math.random() * data.getNumberOfRows()));
+    var dataSet = Math.floor((Math.random() * dataVar.getNumberOfRows()));
 
     var row = [];
 
-    for (var i = 0; i < data.Lf[dataSet].c.length; i++) {
-        row.push(data.Lf[dataSet].c[i].v);
+    var dataSection = dataVar.Lf[dataSet].c;
+    
+
+    if (dataSection == null) {
+        return;
     }
 
-    var valueToChange = Math.floor((Math.random() * row.length)) + 1;
-
-    if (valueToChange >= row.length) {
-        valueToChange = row.length - 1;
+    for (var i = 0; i < dataSection.length; i++) {
+        if (dataSection[i].f != null) {
+            row.push(dataSection[i]);
+            continue;
+        }
+        row.push(dataSection[i].v);
     }
 
+    var valueToChange = null;
 
+    do {
+
+        valueToChange = Math.floor((Math.random() * row.length)) + 1;
+        if (valueToChange >= row.length) {
+            valueToChange = row.length - 1;
+        }
+
+    } while (!isNumeric(row[valueToChange]))
+    
     row[valueToChange] = value;
 
-    data.removeRow(dataSet);
-    data.insertRows(dataSet, [row]);
-
-    chart.draw(data, options);
+    
+        dataVar.removeRow(dataSet);
+        dataVar.insertRows(dataSet, [row]);
+    
+    console.log('random draw');
+    drawChartHelper(chart, dataVar, optionsVar);
 }
 function merge_options(obj1, obj2) {
     var obj3 = {};
     for (var obj1AttrName in obj1) { obj3[obj1AttrName] = obj1[obj1AttrName]; }
     for (var obj2AttrName in obj2) { obj3[obj2AttrName] = obj2[obj2AttrName]; }
     return obj3;
+}
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+var drawing = false;
+function drawChartHelper(chartVar, dataVal, optionsVal) {
+
+    if (drawing) {
+        return;
+    }
+    google.visualization.events.addListener(chartVar, 'ready',
+        function () {
+            drawing = false;
+        });
+    chartVar.draw(dataVal, optionsVal);
 }
