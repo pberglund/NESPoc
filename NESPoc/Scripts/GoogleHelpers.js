14720efc927c0c51bdfefad528c90cac0a33e9bf
Phillip Berglund <pberglund@sagepath.com>
2016-04-28 15:25:16 -0400

    refactored branch and district to use object with function hook property driven chart creation, multiple call support, multiple chart per call support. branch and district working

--- NESPoc/Scripts/GoogleHelpers.js
+++ NESPoc/Scripts/GoogleHelpers.js
@@ -1,6 +1,4 @@
-﻿
-
-function InitializePage(method, callBackFunc, options) {
+﻿function InitializePage(method, callBackFunc, options) {
 
     var opts = options || {};
 
@@ -15,6 +13,116 @@ function InitializePage(method, callBackFunc, options) {
     $.post(url).done(function (d) {
         callBackFunc(d);
     });
+}
+
+function ConfigureCharts(opts) {
+
+    if (opts == null) {
+        return;
+    }
+
+    var currentChartObjs = [];
+
+    for (var i = 0; i < opts.length; i++) {
+
+        var options = opts[i];
+
+        var baseUrl = options["BaseUrl"] || '/Google/';
+
+        var action = options["method"] || 'GraphData';
+
+        var t = '?type=' + (options["type"] || 0);
+
+        var url = baseUrl + action + t;
+
+        $.post(url).done(function (d) {
+            var preProcessing = options["preProcessing"] || function(raw) { return { data: raw };};
+            var pre = preProcessing(d);
+            var data = options["mainManipulation"](pre);
+            var chartObjs = options["postProcessing"](data);
+
+            for (var j = 0; j < chartObjs.length; j++) {
+                var chartType = chartObjs[j].chartType;
+                chartObjs[j].chart = new google.visualization[chartType](document.getElementById(chartObjs[j].elementId));
+                currentChartObjs.push(chartObjs[j]);
+            }
+
+            if (i == opts.length) {
+
+                $(window).resize(function () {
+                    for (var k = 0; k < currentChartObjs.length; k++) {
+                        drawChartHelper(currentChartObjs[k]);
+                    }
+                });
+
+                $(window).resize();
+            }
+        });
+    }
+}
+
+
+function formatCurrencyValue(value) {
+    return format(0, null, null, value);
+}
+
+function formatCurrency(data, col) {
+    return format(0, data, col, null);
+}
+
+function formatPercentageValue(value) {
+    return format(1, null, null, value);
+}
+function formatPercentage(data, col) {
+    return format(1, data, col, null);
+}
+
+function formatDateValue(value) {
+    return format(2, null, null, value);
+}
+
+function formatDate(data, col) {
+    return format(2, data, col, null);
+}
+
+function format(type, data, col, value) {
+
+    var formatter;
+
+    switch(type) {
+        case 0:
+        default :
+            var currencyFormatter = new google.visualization.NumberFormat({
+                negativeColor: 'red',
+                negativeParens: true,
+                pattern: '$###,###'
+            });
+            formatter = currencyFormatter;
+            break;
+        case 1:
+            var percentFormatter = new google.visualization.NumberFormat(
+                       {
+                           pattern: '##.##%'
+                       });
+            formatter = percentFormatter;
+            break;
+        case 2:
+            var date_formatter = new google.visualization.DateFormat({
+                pattern: "MMM yy"
+            });
+            formatter = date_formatter;
+            break;
+
+    }
+
+    if (value == null && data != null && col != null) {
+        formatter.format(data, col);
+        return data;
+    } else {
+        var formattedVal = formatter.formatValue(value);
+        return formattedVal;
+    }
+
 }
 
 function merge_options(obj1, obj2) {
@@ -49,4 +157,24 @@ function GetMagnitude(val) {
         Magnitude: n,
         Remainder: formattedVal,
     };
+}
+
+function goclone(source) {
+    if (Object.prototype.toString.call(source) === '[object Array]') {
+        var clone = [];
+        for (var i = 0; i < source.length; i++) {
+            clone[i] = goclone(source[i]);
+        }
+        return clone;
+    } else if (typeof (source) == "object") {
+        var clone = {};
+        for (var prop in source) {
+            if (source.hasOwnProperty(prop)) {
+                clone[prop] = goclone(source[prop]);
+            }
+        }
+        return clone;
+    } else {
+        return source;
+    }
 }
